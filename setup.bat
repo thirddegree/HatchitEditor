bdea587e57ea21db19c515488d4636161f9b7d45
MattGuerrette <direct3Dtutorials@gmail.com>
2016-10-14 00:38:33 -0400

    added first version on windows build.

---
+++ setup.bat
@@ -0,0 +1,12 @@
+@echo off
+
+IF NOT EXIST  %~dp0build mkdir build
+
+cd build
+
+IF NOT EXIST %cd%ThirdParty mkdir ThirdParty
+
+cd ThirdParty
+cmake ../../Hatchit/ThirdParty -G "Visual Studio 14 2015 Win64" -DDIRECTX=NO -DASSIMP_BUILD_ASSIMP_TOOLS=NO
+msbuild.exe ThirdPartyLibs.sln /p:Configuration=Debug
+msbuild.exe ThirdPartyLibs.sln /p:Configuration=Release
